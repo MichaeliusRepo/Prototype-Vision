2f5f65cf9286a8f81c5ee14a2d5bf9ab5cd6fd3e
MichaeliusDenModige <s143049@student.dtu.dk>
2018-10-30 15:25:27 +0100

    Squares and Toggle Vision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rigidbody;
+    public int HorizontalSpeed;
+    public int VerticalJumpSpeed;
+
+    private bool grounded = false;
+
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+        grounded = (rigidbody.velocity.y == 0);
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        var x = HorizontalSpeed * Input.GetAxis("Horizontal");
+        var y = (Input.GetKey(KeyCode.UpArrow) && grounded) ? VerticalJumpSpeed : rigidbody.velocity.y;
+        rigidbody.velocity = new Vector2(x, y);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerVisionController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisionController : MonoBehaviour {
+
+    private bool Vision;
+
+	// Use this for initialization
+	void Start () {
+        Vision = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vision = (Input.GetKey(KeyCode.T));
+    }
+
+    private void LateUpdate()
+    {
+        var environments = GameObject.FindGameObjectsWithTag("Environment");
+        foreach (var en in environments)
+        {
+            var spriteRenderer = en.gameObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+                spriteRenderer.enabled = !Vision;
+        }
+
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var enemy in enemies)
+            enemy.gameObject.GetComponent<SpriteRenderer>().enabled = Vision;
+
+    }
+}
