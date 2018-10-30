9fd25271b09fdba077d1cf54bd6d30a635fadfc5
MichaeliusDenModige <s143049@student.dtu.dk>
2018-10-30 17:03:53 +0100

    Added Tilemaps and Disable Vision checkbox for debugging

--- Assets/Scripts/PlayerVisionController.cs
+++ Assets/Scripts/PlayerVisionController.cs
@@ -1,27 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class PlayerVisionController : MonoBehaviour {
+public class PlayerVisionController : MonoBehaviour
+{
 
-    private bool Vision;
+    public float ForceTime;
 
-	// Use this for initialization
-	void Start () {
-        Vision = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool DisableVisionFeatures;
+
+
+    private bool Vision = false;
+    private bool visionChanged;
+    private float timeLeft = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!DisableVisionFeatures)
+            UpdateVisibility();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void FixedUpdate()
     {
-        Vision = (Input.GetKey(KeyCode.T));
+        bool input = Input.GetKey(KeyCode.T);
+
+        if (timeLeft <= 0 && Vision != input)
+        {
+            visionChanged = true;
+            Vision = input;
+        }
     }
 
     private void LateUpdate()
+    {
+        if (DisableVisionFeatures) return; // For debugging reasons
+
+        if (visionChanged)
+        {
+            UpdateVisibility();
+
+            visionChanged = false;
+            timeLeft = ForceTime;
+        }
+        if (timeLeft > 0 )
+            timeLeft -= Time.deltaTime;
+
+    }
+
+    private void UpdateVisibility()
     {
         var environments = GameObject.FindGameObjectsWithTag("Environment");
         foreach (var en in environments)
@@ -29,11 +63,14 @@ public class PlayerVisionController : MonoBehaviour {
             var spriteRenderer = en.gameObject.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null)
                 spriteRenderer.enabled = !Vision;
+
+            var tilemapRenderer = en.gameObject.GetComponent<TilemapRenderer>();
+            if (tilemapRenderer != null)
+                tilemapRenderer.enabled = !Vision;
         }
 
         var enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (var enemy in enemies)
             enemy.gameObject.GetComponent<SpriteRenderer>().enabled = Vision;
-
     }
 }
