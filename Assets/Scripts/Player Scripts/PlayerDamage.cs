6c14521e517a8cd35c47a7d3b6212081cc068a7f
MichaeliusDenModige <s143049@student.dtu.dk>
2018-11-06 15:43:24 +0100

    Enemy Collision Detection

---
+++ Assets/Scripts/Player Scripts/PlayerDamage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDamage : MonoBehaviour {
+
+    public Rigidbody2D rg;
+
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 9 || other.gameObject.layer == 10) // 9 is Enemy Layer, 10 is Traps
+        {
+            //Debug.Log("Oh yeah baby!");
+            gameObject.SetActive(false);
+
+            //other.gameObject.SetActive(false);
+
+            RestartGame();
+        }
+
+    }
+
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
