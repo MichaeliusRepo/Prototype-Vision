2d92883ac848e0975ffb84286d52d0cdc26043c3
Rangewaver <Rangewaver@gmail.com>
2018-11-13 16:31:51 +0100

    Updated enemy movement options

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour {
 
     public Rigidbody2D rg;
-    public int speed;
+    public int speed = 2f;
+    public float floordistance = 0.5f;
+    public float hitdistance = 0.05f;
+    public LayerMask terrain;
+    public LayerMask entities;
     public bool horizontal = true;
     public bool restricted = false;
-    public float floor;
-    public LayerMask Terrain;
+    public bool friendhit = false;
+    public bool gravity = true;
+    int dir = 1;
 
 
     //private bool goingRight;
@@ -17,6 +22,8 @@ public class EnemyMovement : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rg = GetComponent<Rigidbody2D>();
+        if (gravity == false)
+            rg.gravityScale = 0;
         //goingRight = true;
 	}
 	
@@ -29,26 +36,63 @@ public class EnemyMovement : MonoBehaviour {
     private void FixedUpdate()
     {
         if (horizontal == true) {
-            if (rg.velocity.x == 0)
+            if (rg.velocity.x == 0) {
                 speed = -speed;
+                dir = dir * -1;
+            }
+            
+
+            Vector2 origin = rg.position;
 
             if (restricted == true) {
 
-                Vector2 origin = rg.position;
+                
 
-                RaycastHit2D left = Physics2D.Raycast(new Vector2(origin.x - 0.5f, origin.y), Vector2.down, floor, Terrain);
-                RaycastHit2D right = Physics2D.Raycast(new Vector2(origin.x + 0.5f, origin.y), Vector2.down, floor, Terrain);
+                RaycastHit2D downleft = Physics2D.Raycast(new Vector2(origin.x - 0.5f, origin.y), Vector2.down, floordistance, terrain);
+                RaycastHit2D downright = Physics2D.Raycast(new Vector2(origin.x + 0.5f, origin.y), Vector2.down, floordistance, terrain);
 
-                Debug.DrawRay(new Vector2(origin.x - 0.5f, origin.y), new Vector2(0, -floor), Color.black);
-                Debug.DrawRay(new Vector2(origin.x + 0.5f, origin.y), new Vector2(0, -floor), Color.black);
+                
 
-                if (left.collider == false)
+                Debug.DrawRay(new Vector2(origin.x - 0.5f, origin.y), new Vector2(0, -floordistance), Color.black);
+                Debug.DrawRay(new Vector2(origin.x + 0.5f, origin.y), new Vector2(0, -floordistance), Color.black);
+
+                
+
+                if (downleft.collider == false)
+                {
                     speed = -speed;
+                    dir = dir * -1;
+                }
 
-                if (right.collider == false)
+                if (downright.collider == false)
+                {
                     speed = -speed;
+                    dir = dir * -1;
+                }
+                
 
             }
+
+            if (friendhit == false) {
+
+                Vector2 sidewalk = new Vector2(1*dir, 0);
+                RaycastHit2D topcheck = Physics2D.Raycast(new Vector2(origin.x +.55f*dir, origin.y + .4f), sidewalk, hitdistance, entities);
+                RaycastHit2D midcheck = Physics2D.Raycast(new Vector2(origin.x +.55f*dir, origin.y), sidewalk, hitdistance, entities);
+                RaycastHit2D botcheck = Physics2D.Raycast(new Vector2(origin.x +.55f*dir, origin.y - .4f), sidewalk, hitdistance, entities);
+
+                Debug.DrawRay(new Vector2(origin.x +.55f*dir, origin.y + .4f), sidewalk, Color.black);
+                Debug.DrawRay(new Vector2(origin.x +.55f*dir, origin.y), sidewalk, Color.black);
+                Debug.DrawRay(new Vector2(origin.x +.55f*dir, origin.y - .4f), sidewalk, Color.black);
+
+                if (topcheck.collider == true || midcheck.collider == true || botcheck.collider == true) {
+
+                    speed = -speed;
+                    dir = dir*-1;
+
+                }
+
+            }
+                
 
             rg.velocity = new Vector2(speed, rg.velocity.y);
         }
