84eb8944d0a281d8d6319f3b07eb6efe4fbd4b55
Rangewaver <Rangewaver@gmail.com>
2018-11-27 13:47:23 +0100

    Added goal scripts + key script (TouchInactive)

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+
+    SpriteRenderer sr;
+    public string sceneName;
+    bool doorOpen = false;
+    public bool trigger = true;
+    GameObject[] target;
+    public string objective = "Key";
+
+
+    // Use this for initialization
+    void Start () {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+                                     	
+	}
+
+    private void FixedUpdate()
+    {
+        if (trigger == true)
+        {
+
+            target = GameObject.FindGameObjectsWithTag(objective);
+            bool cleared = false;
+
+
+            Debug.Log(target.Length);
+            if (target.Length == 0)
+                doorOpen = true;
+                        
+        }
+
+        if (trigger == false)
+            doorOpen = true;
+
+
+        if (doorOpen == false | target == null)
+            sr.color = Color.black;
+        if (doorOpen == true)
+            sr.color = Color.yellow;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (doorOpen)
+        {
+
+            if (collision.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/TouchInactive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInactive : MonoBehaviour {
+
+    public string triggerTag = "Player";
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == triggerTag)
+            gameObject.active = false;
+    }
+
+}
