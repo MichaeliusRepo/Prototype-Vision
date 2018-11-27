899eec641e7a82e875794476b6c00a907ced787b
MichaeliusDenModige <s143049@student.dtu.dk>
2018-11-27 15:41:16 +0100

    Added Enemies as Prefabs

--- Assets/Scripts/EnemyMovementRestricted.cs
+++ Assets/Scripts/EnemyMovementRestricted.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyMovementRestricted : MonoBehaviour {
 
-    public Rigidbody2D rg;
+    private Rigidbody2D rg;
     public int speed;
     public float floor;
     public LayerMask Terrain;
